994c91572cd5f67931eed35e352d62b1d5ff69d8
2130064-TruongCaoDoan <23130064@st.hcmuaf.edu.vn>
2026-04-12 16:50:54 +0700

    add thêm chức năng tích sẵn địa chỉ trong trang thanh toán

--- Web/src/main/java/controler/user/ThanhToan/CreateOrder.java
+++ Web/src/main/java/controler/user/ThanhToan/CreateOrder.java
@@ -46,6 +46,9 @@ public class CreateOrder extends HttpServlet {
 
         return "UNKNOWN";
     }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
